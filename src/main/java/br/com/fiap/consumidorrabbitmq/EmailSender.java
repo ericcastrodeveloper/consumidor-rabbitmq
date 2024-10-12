08949bdace92e26ee26d8d7b2ad1b8ff16722784
castr <castro.eric@hotmail.com>
2024-10-12 20:06:25 -0300

    updating libs

--- src/main/java/br/com/fiap/consumidorrabbitmq/EmailSender.java
+++ src/main/java/br/com/fiap/consumidorrabbitmq/EmailSender.java
@@ -1,12 +1,12 @@
 package br.com.fiap.consumidorrabbitmq;
 
 import br.com.fiap.consumidorrabbitmq.model.InfoMensagem;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import javax.mail.internet.MimeMessage;
 
 @Component
 public class EmailSender {
